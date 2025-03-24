a3bec0f24f0572df1c9c5afbaad6506a528eb2a2
helgithorskarp <166755949+helgithorskarp@users.noreply.github.com>
2025-03-24 14:08:51 +0000

    colors added :D

--- UI.py
+++ UI.py
@@ -9,16 +9,17 @@ class UI:
         self.users = Users()
 
     def gameMenu(self) -> None:
-        userName = self.__loginMenu()
+        userName = self.__loginMenu() 
 
         hasUserQuit = False
-        while not hasUserQuit:
-            hasUserQuit = self.__displayMainScreen(userName)
+        while not hasUserQuit: # keeps going until user enters 'q'
+            hasUserQuit = self.__displayMainScreen(userName) # main menu
             if hasUserQuit:
-                continue
+                continue # user pressed 'q'
+            
             letterCount, guessCount = self.__getGameSettings()
             try:
-                secret = self.wordleEngine.getWord(letterCount)
+                secret = self.wordleEngine.getWord(letterCount) # get random word from bank
                 hasUserQuit = self.__startGameMenu(secret, letterCount, guessCount, userName)
             except NoWordExists:
                 self.__printScreen(
@@ -33,10 +34,11 @@ class UI:
         )
 
     def __loginMenu(self) -> str:
+        '''Allows user to login or create new user, either way the username of logged in account is returned'''
         userDict = self.users.usersDict
 
         option = ''
-        while option not in ['1', '2']:
+        while option not in ['1', '2']: # user has two options either to add a new user or login
             self.__printScreen(
                 '\n🚀 Welcome to the Ultimate PA6 Wordle Challenge! 🌟\n\n'
                 '1. Add a new user ✨\n'
@@ -45,7 +47,7 @@ class UI:
             )
             option = input()
 
-        if option == '2':
+        if option == '2': # user picked to login, keep asking until a valid username is entered
             username = ''
             while username not in userDict:
                 self.__printScreen(
@@ -57,7 +59,7 @@ class UI:
 
         errMessage = ''
         username = ''
-        while not username or username in userDict:
+        while not username or username in userDict: # ask for a new usedName until a unique name is chosen
             self.__printScreen(
                 '\n🔥 Ready to test your Wordle skills? 🔥\n'
                 'Enter your username to create an account and begin your epic adventure! 🏆\n',
@@ -70,6 +72,7 @@ class UI:
         return username
 
     def __displayMainScreen(self, name: str) -> bool:
+        '''Main menu allows user to start game, or navigate menu, true is returned if user quits, else true'''
         highScore = round(self.users.usersDict[name].maxScore, 3)
         userChoice = 'h'
         #Color codes
@@ -77,8 +80,8 @@ class UI:
         YELLOW = "\033[93m"
         RESET = "\033[0m"
 
-        while userChoice.lower() in ['h', 'w']:
-            self.__printScreen(
+        while userChoice.lower() in ['h', 'w']: # keep asking the user for a option while he decides to look at history or add a word
+            self.__printScreen(                 # this is done so that after user returns from either menu they are still at the main menu
                 f"""
     🎉 Welcome to Wordle, {name}!
     🏆 Your High Score: {highScore}  |  🌟 World Record: {round(self.users.recordScore, 3)}
@@ -94,17 +97,19 @@ class UI:
 
             userChoice = input().strip()
             if userChoice.lower() == 'h':
-                self.__showGameHistory(name)
+                self.__showGameHistory(name) # user decides to look at game history menu
             elif userChoice.lower() == 'w':
-                self.__addWordToWordBank()
+                self.__addWordToWordBank() # user decides to look add a new word
 
         return True if userChoice.lower() == 'q' else False
 
     # --- Game Settings & Session ---
     def __getGameSettings(self) -> Tuple[int, int]:
+        '''Asks user for both word length and the letter count of the game, a tuple is returned with both (int(wordLength, int(guessCount))'''
+        
         validWordLength = False
         errMessage = ''
-        while not validWordLength:
+        while not validWordLength: # ask user fot a word length until a valid word length is entered
             self.__printScreen('\n🎲 Set word length (3–8): 🎯\n', '👉 Your length: ', errMessage)
             userWordLength = input()
             if self.wordleEngine.validateWordLength(userWordLength):
@@ -115,7 +120,7 @@ class UI:
 
         validGuessCount = False
         errMessage = ''
-        while not validGuessCount:
+        while not validGuessCount:  # ask user fot a guess count until a valid word length is entered
             self.__printScreen('\n🎰 Set guess count (1–10): 🎲\n', '👉 Your guess count: ', errMessage)
             userGuessCount = input()
             if self.wordleEngine.validateGuessCount(userGuessCount):
@@ -127,51 +132,63 @@ class UI:
         return (int(userWordLength), int(userGuessCount))
 
     def __startGameMenu(self, secret: str, lettersCount: int, guessesAllowed: int, name: str) -> bool:
+        '''plays the wordle game, true is returned if user quits else false'''
         isWinner = False
         guessScore = 0
         errMessage = ''
         guesses: List[Tuple[str, str]] = []
+        
+        #main body that is printed
         body = f'Welcome To Wordle {name}\nPlease guess a {lettersCount} letter word!'
+        
+        # keep playing game until either user wins or the guess score matches the total allowed guesses
         while not isWinner and guessScore != guessesAllowed:
             self.__printScreen('\n\n' + body + '\n\n', f'{lettersCount}-letter word: ', errMessage)
-            userWord = input()
-
+            userWord = input() # ask for a word
+            
+            # if word is invalid then restart while loop
             if not self.wordleEngine.validateGuess(userWord, lettersCount):
                 errMessage = f'Your guess must only contain letters and be of length {lettersCount}\n'
                 continue
 
+            # get hint from engine based on guess, if the hint is a winning hint then while loop ends
             hint = self.wordleEngine.generateHint(secret, userWord)
             guesses.append((hint, userWord))
             if self.wordleEngine.isWinner(hint, lettersCount):
                 guessScore += 1
-                isWinner = True
+                isWinner = True # declared as winner
                 continue
 
             errMessage = ''
             guessScore += 1
-            body = self.__generateBody(guesses)
+            body = self.__generateGameBody(guesses) # update body for next round
 
+        # after game is finished new record is stored and closing menu is displayed
         self.users.addNewRecordToUser(name, lettersCount, guessesAllowed, guessScore, isWinner)
         return self.__closeGameMenu(isWinner, guesses, secret)
 
-    def __generateBody(self, guesses: List[Tuple[str, str]]) -> str:
+    def __generateGameBody(self, guesses: List[Tuple[str, str]]) -> str:
         '''Generate game body text with previous guesses and hints.'''
         return '\n'.join([f'{guess[0]}       {guess[1].upper()}' for guess in guesses])
 
     def __closeGameMenu(self, didUserWin: bool, guesses: List[Tuple[str, str]], secret: str) -> bool:
-        body = self.__generateBody(guesses)
+        '''Shows closing menu after a game has been finished, true is returned if user quits, true if not'''
+        body = self.__generateGameBody(guesses)
 
         if didUserWin:
             body += f'\n\n🥳 Woohoo! You nailed it! 🎉\nYour score: {len(secret) ** 2 / len(guesses):.2f} 🚀'
         else:
             body += f'\n\n😅 So close! Better luck next round!\nThe secret word was: {secret.upper()} 📚'
 
-        self.__printScreen(body, '✨ Press enter to play again, or type "q" to quit! 🚪: ')
+        self.__printScreen(body, '✨ Press enter to go to main menu, or type "q" to quit! 🚪: ')
         userInput = input()
         return userInput.lower() == 'q'
 
 
     def __showGameHistory(self, name: str) -> None:
+        '''Shows user his game history nothing is returned'''
+        
+        # body of all games
         gameHistoryList = [
             f"{game}: {points} points, {'win' if points > 0 else 'loss'}" 
             for game, points in self.users.usersDict[name].scores.items()
@@ -181,8 +198,11 @@ class UI:
         input()
 
     def __addWordToWordBank(self) -> None:
+        '''shows word bank menu and allows user to add a new word'''
+        
         newWordFound = False
         errMessage = ''
+        # keep asking user for a word until a valid word is found
         while not newWordFound:
             self.__printScreen(
                 "\n📝 Let's add a new word to your Word Bank!\n🔤 Must be 3–8 letters long and only contain letters.\n",
@@ -193,20 +213,25 @@ class UI:
             newWordFound = self.wordleEngine.validateNewWord(newWord)
             errMessage = '⚠️  Word must only contain letters and be between 3 and 8 characters long.\n'
 
-        self.wordleEngine.addNewWord(newWord)
+        self.wordleEngine.addNewWord(newWord) # add new word to bank, display screen allowing the user to return 
         self.__printScreen(f"\nNew word '{newWord}' has been added to the Word Bank! 🚀\n", "Press enter to continue: ")
         input()
         
 
     # --- Utility Methods PRINTS screen and clears terminal ---
     def __printScreen(self, body: str, inputPrompt: str, errMessage: str = '') -> None:
+        '''Prints screen based on the body and input prompt entered, nothing is returned'''
         self.__clearScreen()
+        RED = '\033[91m'
+        CYAN = '\033[96m'
+        RESET = '\033[0m'
+        
         menu = f'''
------------------------------------------------------------------
-                           WORDLE PA6
------------------------------------------------------------------
+{RED}-----------------------------------------------------------------{RESET}
+{CYAN}                        WORDLE PA6 🎲{RESET}
+{RED}-----------------------------------------------------------------{RESET}
 {body}
------------------------------------------------------------------
+{RED}-----------------------------------------------------------------{RESET}
 {errMessage + inputPrompt}'''
         print(menu, end='')
 
--- wordleEngine.py
+++ wordleEngine.py
@@ -9,56 +9,60 @@ class WordleEngine:
         self.wordDict = self.__readWordBank()
         
     def getWord(self, length: int) -> str:
+        '''returns a word from word bank that matches the length entered, A NoWordExists error is raised if nothing is found '''
         try:
             return random.choice(self.wordDict[length])
         except (KeyError, IndexError):
             raise NoWordExists()
                 
     def __readWordBank(self) -> dict:
+        '''Reads from word bank into a dictionary where keys are word lengths and values are a list of words'''
         words = {}
         with open('wordBank.txt', 'r') as file:
             for word in file:
                 cleanedWord = word.strip().lower()
                 wordLength = len(cleanedWord)
-                if wordLength in words:
+                if wordLength in words: # word length key exists, append to that list
                     if cleanedWord not in words[wordLength]:
                         words[wordLength].append(cleanedWord)
                 else:
-                    words[wordLength] = [cleanedWord]
+                    words[wordLength] = [cleanedWord] # word key does not exist, create new word lenght key
         return words
 
     @staticmethod
     def validateGuess(word: str, lettersCount: int) -> bool:
+        '''validates guess, true is returned if the guess is of correct length and is only letters, else false'''
         return len(word) == lettersCount and word.isalpha()
     
     @staticmethod
     def generateHint(secret: str, guess: str) -> str:
+        '''Generates a wordle hint, C is in correct position, c not correct position, - not in word'''
         secret, guess = secret.lower(), guess.lower()
         hint = ['-'] * len(secret)
         letterCount = {}
 
-        for ch in secret:
+        for ch in secret: # dictionary that counts how many times each letter appears
             if ch in letterCount:
                 letterCount[ch] += 1
             else:
                 letterCount[ch] = 1
 
-        for i in range(len(secret)):
+        for i in range(len(secret)): # loop trough word and add a C if letter is in correct position and also subtract from the dictionary
             if guess[i] == secret[i]:
                 hint[i] = 'C'
                 letterCount[guess[i]] -= 1
 
-        for i in range(len(secret)):
+        for i in range(len(secret)): # loop trough word again, add a lowercase c iff word is in secret and the dictionary number for letter is larger than 0
             if hint[i] != 'C' and guess[i] in letterCount and letterCount[guess[i]] > 0:
                 hint[i] = 'c'
                 letterCount[guess[i]] -= 1
 
-        # Color codes
+        # Color codes 
         GREEN = "\033[92m"
         YELLOW = "\033[93m"
         RESET = "\033[0m"
 
-        coloredHint = ""
+        coloredHint = "" # loop trough the hint to create a colored hint
         for ch in hint:
             if ch == 'C':
                 coloredHint += f"{GREEN}C{RESET}"
@@ -71,16 +75,19 @@ class WordleEngine:
     
     @staticmethod
     def isWinner(wordHint: str, letterCount: int) -> bool:
-        GREEN_C = "\033[92mC\033[0m"
+        '''returns true iff the word hint is all upper case green C's'''
+        GREEN_C = "\033[92mC\033[0m" # this is basically what a green C represents, word is correct if it is this times the word length
         return wordHint == GREEN_C * letterCount
 
     def validateWordLength(self, value: str) -> bool:
+        'validates the word length of a game, returns true iff word length is 3-8 '
         if not value.isdigit():
             return False
         length = int(value)
         return 3 <= length <= 8
 
     def validateGuessCount(self, value: str) -> bool:
+        ''''''
         if not value.isdigit():
             return False
         count = int(value)
